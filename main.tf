1df59594388369c8de6b9e18dd92dbfadac8bc35
Sheriff <realexcel2020@gmail.com>
2023-01-23 17:45:21 +0100

    added a name to the deployed ec2 server

--- main.tf
+++ main.tf
@@ -5,4 +5,8 @@ provider "aws" {
 resource "aws_instance" "test-server" {
     ami = "ami-00874d747dde814fa"
     instance_type = "t2.micro"
+
+    tags = {
+        Name = "Terraform-Test"
+    }
 }
